8a09729a15b2768d4bbd2c5e5f9d2debe8bda53c
omkarjadhav777 <148333479+omkarjadhav777@users.noreply.github.com>
2025-07-01 01:02:48 +0530

    Update Copyright.jsx

--- client/src/components/Copyright.jsx
+++ client/src/components/Copyright.jsx
@@ -6,9 +6,9 @@ export default function Copyright() {
 
   return (
     <Typography mt="-2" variant="body2" align="center" sx={{ color: 'text.secondary'}}>
-            &copy; Sunny George | Open Source &nbsp; 
+            &copy; Omkar Jadhav | Open Source &nbsp; 
             <FavoriteIcon  color="error"  sx={{ fontSize: 18 ,mb:'-2px', ml:'-5px' }}/><br/>
-            <Link variant="subtitle3" component={'a'} href="https://github.com/tuzup/SplitApp/" target="_blank">
+            <Link variant="subtitle3" component={'a'} href="https://github.com/omkarjadhav777" target="_blank">
             [GitHub]
             </Link>
           </Typography>
